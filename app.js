6d0fdee54f97d327c9058af97552cec6b66127d3
bcuser <j.rao@bellevuecollege.edu>
2022-10-23 12:39:59 -0700

    Adds handling HTTP get and post requests for user plants from app

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const cors = require('cors');
+const corsOptions = {
+	origin: 'http://localhost:4200',
+	optionsSuccessStatus: 200
+}
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.use(cors(corsOptions));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,84 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// for this version, we will keep data on server in an array
+userPlantArray =  [{plantUserName: "test"}];
+
+router.get('/userplant', function(req, res) {
+  userPlantArray.forEach(plant => {
+    console.log(plant.plantUserName);
+  });
+  res.status(200).json(userPlantArray);
+})
+
+// router.get('/heroes/:id', function(req, res) {
+//   let found = false;
+//     for(var i=0; i < heroArray.length; i++)
+//     {
+//       if( heroArray[i].id == req.params.id)
+//       {
+//         console.log(heroArray[i]);
+//         found = true
+//         res.status(200).json(heroArray[i]);
+//       }
+//     }
+//     if(found === false){
+//       res.status(500).send("no such hero");
+//       }
+//   });
+
+
+//   router.put('/heroes/:id', function(req, res) {
+//     var changedHero = req.body; 
+//     let found = false;
+//     for(var i=0; i < heroArray.length; i++)
+//     {
+//       if( heroArray[i].id == req.params.id)
+//       {
+//         heroArray[i] = changedHero;
+//         found = true
+//         res.status(200).json(heroArray[i]);
+//       }
+//     }
+//     if(found === false){
+//       res.status(500).send("no such hero");
+//       }
+//    });
+
+
+
+// // delete is used to delete existing object using the user's app id field
+// router.delete('/heroes/:id', function (req, res) {
+//   let found = false;
+//   for(var i=0; i < heroArray.length; i++)
+//   {
+//     if( heroArray[i].id == req.params.id)
+//     {
+//       console.log(heroArray[i]);
+//       found = true
+//       heroArray.splice(i,1);
+//       res.status(200).json(heroArray[i]);
+//     }
+//   }
+//   if(found === false){
+//     res.status(500).send("no such hero");
+//     }
+// });
+
+/* post a new plant  */
+router.post('/userplant', function(req, res) {
+  var newPlant = (req.body);
+  newPlant.id = uid();   // use code below to create unique id
+  console.log("new id = " + newPlant.id);
+  userPlantArray.push(newPlant);
+  res.status(201).json(newPlant);
 });
 
 module.exports = router;
+
+//Create a unique ID using time + random
+const uid = () =>
+  String(
+    Date.now().toString(32) +
+      Math.random().toString(16)
+  ).replace(/\./g, '')
